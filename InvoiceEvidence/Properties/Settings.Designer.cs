a451bf9008386d273ac1f901ff768ecccdf18ce0
Marek Vajgl <engin@seznam.cz>
2024-11-15 17:29:40 +0100

    Migrated to FW 4.8

--- InvoiceEvidence/Properties/Settings.Designer.cs
+++ InvoiceEvidence/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace InvoiceEvidence.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -157,7 +157,7 @@ namespace InvoiceEvidence.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\Vajgl\\OneDrive - Ostravska univerzita\\Synchronized\\Faktury")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Users\\vajgma91\\OneDrive - Ostravská univerzita\\Synchronized\\Faktury")]
         public string LastDatabaseFolder {
             get {
                 return ((string)(this["LastDatabaseFolder"]));
